138a00e324d08a7fe212179e4d4421a960a1c6b2
RAVIRAJ KUKADE <ravirajkukade11@gmail.com>
2023-03-25 16:37:03 +0530

    done hyperparameter

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,8 @@
 import os
 import sys
 from src.exception import CustomException
+from src.logger import logging
+from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import r2_score
 import numpy as np
 import pandas as pd
@@ -17,21 +19,27 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys) from e
     
-def evaluate_models(X_train, y_train, X_test, y_test, models):
-    
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
+    logging.info('trying hyperparameter tunning')
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            model.fit(X_train, y_train)
+            para=param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
 
             train_model_score = r2_score(y_train, y_train_pred)
             test_model_score = r2_score(y_test, y_test_pred)
             report[list(models.keys())[i]] = test_model_score
-        
+        logging.info('done with training and hyperparameter tunning and found the best model')
         return report
     except Exception as e:
         raise CustomException(e, sys) from e
